0e0abbe97f55add2397730c8b2d1c9a86adb329e
PrigoanaClaudiu <claudiu_prigoana16@yahoo.com>
2024-08-20 19:15:24 +0300

    adding del + all cotact

--- app/routers/contact.py
+++ app/routers/contact.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
@@ -31,3 +31,23 @@ def create_contact(contact: schemas.ContactCreate, db: Session = Depends(databas
     db.refresh(new_contact)
 
     return new_contact
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_contact(id: int, db: Session = Depends(database.get_db), 
+                   current_user: models.User = Depends(oauth2.get_current_user)):
+    contact = db.query(models.Contact).filter(models.Contact.id == id).first()
+
+    if not contact:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found")
+
+    db.delete(contact)
+    db.commit()
+
+    return {"message": "Contact deleted successfully"}
+
+# Adăugare rută pentru obținerea tuturor contactelor
+@router.get("/", response_model=List[schemas.Contact])
+def get_contacts(db: Session = Depends(database.get_db), 
+                 current_user: models.User = Depends(oauth2.get_current_user)):
+    contacts = db.query(models.Contact).all()
+    return contacts
